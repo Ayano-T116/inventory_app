57a8fc3a6801dcb1bdaa701cfd61f76941183fce
Ayano-T116 <fdkjy093@gmail.com>
2026-08-06 16:04:29 +0900

    Add keep alive action

---
+++ inventory-app/keepAlive.js
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY_1
+);
+
+const { data, error } = await supabase
+  .from("system_keep_alive")
+  .update({
+    updated_at: new Date().toISOString(),
+  })
+  .eq("id", 1);
+
+if (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+console.log("Keep Alive Success");
